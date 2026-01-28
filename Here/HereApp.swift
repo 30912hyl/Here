5df6517c370d5d92bc969b283aafc7f6f80affb0
Yuumiiiiii <yguo6520@gmail.com>
2026-01-27 22:22:13 -0500

    Initial Commit

---
+++ Here/HereApp.swift
@@ -0,0 +1,17 @@
+//
+//  HereApp.swift
+//  Here
+//
+//  Created by yuchen on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct HereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
